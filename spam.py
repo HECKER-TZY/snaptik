a1c1c5752a0367f2299240d2664f4dff6f2422be
FAJAR-DEV <95860666+HECKER-TZY@users.noreply.github.com>
2022-12-17 17:41:27 +0700

    spam_sms_unli/README.md

---
+++ spam.py
@@ -0,0 +1,7 @@
+pkg update && pkg upgrade
+pkg install python
+pkg install git
+pip install requests
+git clone https//:github.com/HECKER-TZY/spam_sms_unli
+cd spam_sms_unli
+python spam_sms_unli.py
